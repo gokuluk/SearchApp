b6206f23d4a95fd612ef0962c54ed023e469e3a6
unnikris <gokuldx10@gmail.com>
2020-04-01 22:58:45 +0530

    project files

---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import SearchPage from '../searchPage/SearchPage'
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <SearchPage />
+
+            </div>
+        )
+    }
+}
